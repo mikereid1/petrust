8f96bce0b9dec0dd0f22879b5260520d216a1d09
Michael Reid <michaelreid87@protonmail.com>
2024-06-29 19:04:44 +0200

    add logger and format project

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-mod service;
-
+use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
+use env_logger::Env;
+
 use service::config;
+
+mod service;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
 
     HttpServer::new(|| {
-        App::new().configure(config::configure_services)
+        App::new()
+            .wrap(Logger::default())
+            .configure(config::configure_services)
     })
-        .bind(("127.0.0.1", 8080))?
-        .workers(2)
-        .run()
-        .await
+    .bind(("127.0.0.1", 8080))?
+    .workers(2)
+    .run()
+    .await
 }
--- src/service/config.rs
+++ src/service/config.rs
@@ -1,44 +1,45 @@
 use actix_web::web;
-use crate::service::pet::{create_pet, delete_pet, get_pet, update_pet};
 
-use crate::service::store::{create_order, delete_order, get_inventory, get_order};
-use crate::service::user::{create_user, delete_user, get_user, login, logout, update_user};
+use crate::service::{
+    pet::{create_pet, delete_pet, get_pet, update_pet},
+    store::{create_order, delete_order, get_inventory, get_order},
+    user::{create_user, delete_user, get_user, login, logout, update_user},
+};
 
 pub fn configure_services(cfg: &mut web::ServiceConfig) {
-    cfg
-        .service(
-            web::scope("/pet")
-                .route("/", web::post().to(create_pet))
-                .service(
-                    web::resource("/{petId}")
-                        .route(web::get().to(get_pet))
-                        .route(web::put().to(update_pet))
-                        .route(web::delete().to(delete_pet))
-                )
-        )
-        .service(
-            web::scope("/store")
-                .route("/inventory", web::post().to(get_inventory))
-                .service(
-                    web::scope("/order")
-                        .route("/", web::post().to(create_order))
-                        .service(
-                            web::resource("/{orderId}")
-                                .route(web::get().to(get_order))
-                                .route(web::delete().to(delete_order))
-                        )
-                )
-        )
-        .service(
-            web::scope("/user")
-                .route("/", web::post().to(create_user))
-                .route("/login", web::post().to(login))
-                .route("/logout", web::post().to(logout))
-                .service(
-                    web::resource("/{username}")
-                        .route(web::get().to(get_user))
-                        .route(web::put().to(update_user))
-                        .route(web::delete().to(delete_user))
-                )
-        );
+    cfg.service(
+        web::scope("/pet")
+            .route("/", web::post().to(create_pet))
+            .service(
+                web::resource("/{petId}")
+                    .route(web::get().to(get_pet))
+                    .route(web::put().to(update_pet))
+                    .route(web::delete().to(delete_pet)),
+            ),
+    )
+    .service(
+        web::scope("/store")
+            .route("/inventory", web::post().to(get_inventory))
+            .service(
+                web::scope("/order")
+                    .route("/", web::post().to(create_order))
+                    .service(
+                        web::resource("/{orderId}")
+                            .route(web::get().to(get_order))
+                            .route(web::delete().to(delete_order)),
+                    ),
+            ),
+    )
+    .service(
+        web::scope("/user")
+            .route("/", web::post().to(create_user))
+            .route("/login", web::post().to(login))
+            .route("/logout", web::post().to(logout))
+            .service(
+                web::resource("/{username}")
+                    .route(web::get().to(get_user))
+                    .route(web::put().to(update_user))
+                    .route(web::delete().to(delete_user)),
+            ),
+    );
 }
